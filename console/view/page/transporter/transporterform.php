385155a7f0507c7d4ad0e7a6ababcab08711cbab
nishajasoliya17 <64001510+nishajasoliya17@users.noreply.github.com>
2021-09-10 21:09:59 +0530

    Create transporterform.php

---
+++ console/view/page/transporter/transporterform.php
@@ -0,0 +1,90 @@
+<div class="card mb-3">
+    <div class="card-header">
+        <i class="fa fa-table"></i>
+        Form
+    </div>
+    <div class="col-lg-10">
+        <div class="card-body">
+            <?php
+                if(!isset($_REQUEST['id']))
+                {
+
+                
+            ?>
+            <h1>ADD FORM</h1>
+            <form method="post" action="index.php?controller=transportercontroller&action=add" enctype=multipart/form-data>
+                <div class="form-group">
+                    <label>Name:</label>
+                    <input type="text" name="name" class="form-control" required>
+                </div>
+
+                <div class="form-group">
+                    <label>Mobile Number:</label>
+                    <input type="text" name="mobileno"  class="form-control" required>
+                </div>
+
+                <div class="form-group">
+                    <label>Password:</label>
+                    <input type="password" name="password" class="form-control" required>
+                </div>
+
+                <div class="form-group">
+                    <label>Image:</label>
+                    <input type="file" name="f" class="form-control">
+                </div>
+
+                <div class="form-group">
+                    <input type="submit" value="ADD" class="btn btn-info">
+                </div>
+            </form>
+
+                <?php } 
+                else
+                {
+                    foreach($row as $val)
+                    {
+                ?>
+                <h1>EDIT FORM</h1>
+
+                <form method="post" action="index.php?controller=transportercontroller&action=edit" enctype=multipart/form-data>
+                <div class="form-group">
+                    <label>Name:</label>
+                    <input type="text" name="name" class="form-control" value="<?php echo $val['name']; ?>" required>
+                </div>
+
+                <div class="form-group">
+                    <label>Mobile Number:</label>
+                    <input type="text" name="mobileno" class="form-control" value="<?php echo $val['mobileno']; ?>" required>
+                </div>
+
+                <div class="form-group">
+                    <label>Password:</label>
+                    <input type="password" name="password" class="form-control" value="<?php echo $val['password']; ?>" required>
+                </div>
+
+                 <div class="form-group">
+                    <label>Previos Image</label>
+                    <img src="<?php echo $val['img']; ?>" style="height: 100px;width:100px;">
+
+                </div>
+
+                <div class="form-group">
+                    <label>Image:</label>
+                    <input type="file" name="f" class="form-control" accept="image/*">
+                </div>
+
+                <div class="form-group">
+                    <input type="submit" value="EDIT" class="btn btn-info">
+                </div>
+
+                <input type="hidden" name="id" value="<?php echo $val['id'] ?>" >
+                <input type="text" name="oldpath" value="<?php echo $val['img']; ?>">
+                <input type="text" name="oldpath" value="<?php echo $val['img']; ?>">
+            </form>
+
+
+                <?php } } ?>
+        </div>
+    </div>
+</div>
+
